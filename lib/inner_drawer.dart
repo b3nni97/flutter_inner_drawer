5aad3e217f8bc5fae527b04fe66ff3a1ac0e2f95
Benjamin Schweizer <bs103@hdm-stuttgart.de>
2024-03-03 21:15:43 +0100

    # Update

--- lib/inner_drawer.dart
+++ lib/inner_drawer.dart
@@ -628,7 +628,7 @@ class InnerDrawerState extends State<InnerDrawer>
     return Container(
       decoration: widget.backgroundDecoration ??
           BoxDecoration(
-            color: Theme.of(context).backgroundColor,
+            color: Theme.of(context).scaffoldBackgroundColor,
           ),
       child: Stack(
         alignment: _drawerInnerAlignment(_position)!,
